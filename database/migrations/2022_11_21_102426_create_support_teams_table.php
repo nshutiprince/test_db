aef010e664d2103ca4990409a14f71e8230e5821
nshutiprince <75612008+nshutiprince@users.noreply.github.com>
2022-11-21 12:29:07 +0200

    first commit

---
+++ database/migrations/2022_11_21_102426_create_support_teams_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupportTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('support_teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('names');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('support_teams');
+    }
+}
---
+++ database/migrations/2022_11_21_102440_create_court_cases_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourtCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('court_cases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('crime_id');
+            $table->unsignedBigInteger('plainitiff_id');
+            $table->unsignedBigInteger('judge_id')->nullable();
+            $table->string('subject');
+            $table->text('description')->nullable();
+            $table->string('representative')->nullable();
+            $table->string('attachment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('court_cases');
+    }
+}
